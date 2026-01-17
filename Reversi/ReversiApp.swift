da3ae94eb5f817018110acbbbfd09dbb58d88047
oartemiy <artemiy.obraztsov@yandex.ru>
2026-01-17 20:29:19 +0300

    Initial Commit

---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by Артемий Образцов on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
