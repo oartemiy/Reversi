4791a5bb26abf44580a7b9ba69c6e0ca8d887075
oartemiy <artemiy.obraztsov@yandex.ru>
2026-01-18 22:11:42 +0300

    Added animation for field and started settings screen

---
+++ Reversi/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewModel.swift
+//  Reversi
+//
+//  Created by Артемий Образцов on 18.01.2026.
+//
+
+import Foundation
---
+++ Reversi/Views/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  Reversi
+//
+//  Created by Артемий Образцов on 18.01.2026.
+//
+
+import Foundation
